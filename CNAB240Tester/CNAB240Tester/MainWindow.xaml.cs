0a606ec661c974a719bcca7627fef442985dbe50
Neto <ironeto@hotmail.com>
2017-04-30 10:01:37 -0300

    .

--- CNAB240Tester/CNAB240Tester/MainWindow.xaml.cs
+++ CNAB240Tester/CNAB240Tester/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -28,7 +29,59 @@ namespace CNAB240Tester
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            var ret = new FileProcessing().ImportFile(new StreamReader(@"C:\Teste\CNAB-240 BB.txt").BaseStream, Convert.ToDecimal(tbxSaldo.Text));
+            try
+            {
+                if(string.IsNullOrEmpty(tbxSaldo.Text))
+                {
+                    MessageBox.Show("Por favor, informe o saldo.");
+                    return;
+                }
+
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.DefaultExt = "*.txt";
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    var ret = new FileProcessing().ImportFile(new StreamReader(openFileDialog.FileName).BaseStream, Convert.ToDecimal(tbxSaldo.Text));
+
+                    switch (ret.Status.Code)
+                    {
+                        case CNAB240BB.DeliveryFile.Cnab240EnumCodes.ProcessingFile:
+                            MessageBox.Show("Erro em: Processando arquivo...");
+                            break;
+                        case CNAB240BB.DeliveryFile.Cnab240EnumCodes.InvalidFileFormat:
+                            MessageBox.Show("Arquivo com formato inválido");
+                            break;
+                        case CNAB240BB.DeliveryFile.Cnab240EnumCodes.InvalidFormatPaymentValue:
+                            MessageBox.Show("Formato de valor inválido");
+                            break;
+                        case CNAB240BB.DeliveryFile.Cnab240EnumCodes.CompanyNotFound:
+                            MessageBox.Show("Cliente não encontrado");
+                            break;
+                        case CNAB240BB.DeliveryFile.Cnab240EnumCodes.UserNotFound:
+                            MessageBox.Show("Funcionário não encontrado");
+                            break;
+                        case CNAB240BB.DeliveryFile.Cnab240EnumCodes.ErroProcessingLine:
+                            MessageBox.Show("Erro ao processar arquivo");
+                            break;
+                        case CNAB240BB.DeliveryFile.Cnab240EnumCodes.CreditInsertedSuccessfully:
+                            MessageBox.Show("Crédito realizado com sucesso!");
+                            break;
+                        case CNAB240BB.DeliveryFile.Cnab240EnumCodes.InsufficientFunds:
+                            MessageBox.Show("Fundos insuficientes");
+                            break;
+                        case CNAB240BB.DeliveryFile.Cnab240EnumCodes.FileSuccessfullyProcessed:
+                            MessageBox.Show("Arquivo processado com sucesso");
+                            break;
+                        default:
+                            MessageBox.Show("Retorno não tratado!!!");
+                            break;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
